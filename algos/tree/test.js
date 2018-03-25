1998056fc6d1cc28b8a57d487e7c6a61ab047995
avamrocking <avamrocking@gmail.com>
2018-03-25 10:01:49 -0400

    changes

--- algos/tree/test.js
+++ algos/tree/test.js
@@ -1,4 +1,5 @@
 const T = require('./index');
+const levelWidth = require('./levelwidth');
 const Node = T.Node;
 const Tree = T.Tree;
 
@@ -63,5 +64,32 @@ describe('Tree', () => {
     });
 
     expect(letters).toEqual(['a', 'b', 'c', 'd']);
+  });
+});
+
+describe('LevelWidth',()=> {
+  test('levelWidth is a function', () => {
+    expect(typeof levelWidth).toEqual('function');
+  });
+
+  test('levelWidth returns number of nodes at widest point', () => {
+    const root = new Node(0);
+    root.add(1);
+    root.add(2);
+    root.add(3);
+    root.children[0].add(4);
+    root.children[2].add(5);
+  
+    expect(levelWidth(root)).toEqual([1, 3, 2]);
+  });
+
+  test('levelWidth returns number of nodes at widest point', () => {
+    const root = new Node(0);
+    root.add(1);
+    root.children[0].add(2);
+    root.children[0].add(3);
+    root.children[0].children[0].add(4);
+  
+    expect(levelWidth(root)).toEqual([1, 1, 2, 1]);
   });
 });
